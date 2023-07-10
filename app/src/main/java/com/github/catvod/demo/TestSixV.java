0a86479f39567d49a22316f8459d529b1f9a0969
zhixc <eg79@qq.com>
2023-07-10 12:07:39 +0800

    更新6V电影网

--- app/src/main/java/com/github/catvod/demo/TestSixV.java
+++ app/src/main/java/com/github/catvod/demo/TestSixV.java
@@ -3,36 +3,49 @@ package com.github.catvod.demo;
 import android.content.Context;
 
 import com.github.catvod.spider.SixV;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TestSixV {
-    public static void main(String[] args) {
-        SixV sixV = new SixV();
-        sixV.init(new Context(), "https://www.66ss.org");
+    SixV sixV;
+
+    @Before
+    public void init() {
+        sixV = new SixV();
+        sixV.init(new Context(), "https://www.6vdy.org/");
+    }
+
+    @Test
+    public void homeContent() {
         // 首页测试，输出...
-//        String s = sixV.homeContent(true);
-//        System.out.println(s);
+        String s = sixV.homeContent(true);
+        System.out.println(s);
+    }
 
+    @Test
+    public void categoryContent() {
         // 分类页面数据测试
-//        String s = sixV.categoryContent("my_tid_movie", "1", true, new HashMap<>());
-//        System.out.println(s);
+        HashMap<String, String> extend = new HashMap<>();
+        System.out.println(sixV.categoryContent("my_tid_movie", "1", true, extend));
+    }
 
+    @Test
+    public void detailContent() {
         // 详情页面数据测试
         ArrayList<String> ids = new ArrayList<>();
-//        ids.add("https://www.66ss.org/xijupian/20346.html");
-//        ids.add("https://www.66ss.org/xijupian/20531.html");
-        ids.add("https://www.66ss.org/xijupian/20620.html");
-        // ids.add("https://www.66s.cc/dongzuopian/21174.html");
-        // ids.add("https://www.66s.cc/donghuapian/21001.html");
-        ids.add("https://www.66s.cc/donghuapian/20991.html");
-        String s = sixV.detailContent(ids);
-        System.out.println(s);
+        ids.add("https://www.6vdy.org/xijupian/20346.html");
+//        ids.add("https://www.6vdy.org/xijupian/20531.html");
+//        ids.add("https://www.6vdy.org/xijupian/20620.html");
+        System.out.println(sixV.detailContent(ids));
+    }
 
+    @Test
+    public void searchContent() {
         // 搜索测试
-//        String s = sixV.searchContent("保镖", true);
-//        System.out.println(s);
-
+        String s = sixV.searchContent("保镖", true);
+        System.out.println(s);
     }
 }
